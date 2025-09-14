e84153f5207b087ba0ee80ee96215d03f6aa076b
Punyasyitha <masyitharala29@gmail.com>
2025-09-14 16:40:50 +0700

    add dark mode  to guest layout

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,6 +1,6 @@
 {{-- resources/views/layouts/guest.blade.php --}}
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="scroll-smooth dark">
 
 <head>
     <meta charset="utf-8">
